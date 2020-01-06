9ae615f2570f6cbee85a265ce57bc45abd7c22ca
David-el Yosef <52068440+davidelyosef@users.noreply.github.com>
2020-01-06 20:18:45 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function (config) {
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
+    }, 
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/angularProject'),
       reports: ['html', 'lcovonly', 'text-summary'],
